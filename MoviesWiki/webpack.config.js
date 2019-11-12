2d6f9b130295a899024af907e4254d83b0b0a42d
MINSK\Aliaksei_Kazlou <alexkozlov11g11@gmail.com>
2019-11-12 21:03:09 +0300

    Add minification js

--- MoviesWiki/webpack.config.js
+++ MoviesWiki/webpack.config.js
@@ -1,16 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+let plugins = [new HtmlWebpackPlugin({
+      template: "./index.html"
+    })]
+
+let optimazers = new UglifyJsPlugin();
+
+if(process.env.NODE_ENV === 'production') {
+	//plugins.push(new BabelMinifyWebpackPlugin());
+	//optimazers.push(new UglifyJsPlugin());
+}
 
 module.exports = {
 	context: path.join(__dirname, 'src'),
 	entry: './index.js',
-	
-	devtool: 'source-map',
+		
+	devtool: process.env.NODE_ENV === 'production' ? 'none' : 'source-map',
 	
 	output: {
 		filename: 'bundle.js',
 		path: path.join(__dirname, 'dist'),
 	},
+	
+	mode: process.env.NODE_ENV === 'production' ? "production": "development",
+	
+	optimization: process.env.NODE_ENV === 'production' ? { minimizer: [optimazers], } : { },
 	
 	module: {
 		rules: [
@@ -32,9 +48,7 @@ module.exports = {
 		extensions: ['.js']
 	},
 	
-	plugins: [new HtmlWebpackPlugin({
-      template: "./index.html"
-    })],
+	plugins: plugins,
 	
 	watch: false
 };
