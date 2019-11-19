f75e57ea5cfcb868993a87b15de7681b3dd862a4
MINSK\Aliaksei_Kazlou <alexkozlov11g11@gmail.com>
2019-11-19 14:24:17 +0300

    Update Footer

--- MoviesWiki/src/components/Footer.js
+++ MoviesWiki/src/components/Footer.js
@@ -2,14 +2,10 @@ import React, { Component } from "react";
 
 import styles from "../styles/Footer.css"
 
-class Footer extends Component {
-    render() {
-        return (
-            <div className={styles.footer}>
-                <div className={styles.container}>{this.props.config.title}</div>        
-            </div>
-        );
-    }
-}
+function Footer(props) {
+    return <div className={styles.footer}>
+                <div className={styles.container}>{props.config.title}</div>        
+            </div>;
+  }
 
 export default Footer;
