68ba62706b57fb44b1d9b2850a745eaf21fd8b23
MINSK\Aliaksei_Kazlou <alexkozlov11g11@gmail.com>
2019-11-26 17:25:26 +0300

    Draft updated test for CustomOptionList

--- MoviesWiki/src/components/Common/CustomOptionList.test.js
+++ MoviesWiki/src/components/Common/CustomOptionList.test.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import { shallow, mount, configure } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
 
 import CustomOptionList from "./CustomOptionList";
+import styles from "../../styles/CustomOptionList.css"
 
 configure({adapter: new Adapter()});
 
-test('CustomOptionList rendering test', () => {
+describe('CustomOptionList component', () => {
 
-    var config = {
-        title: "Search by",
-        selectedIndex: "0",
-        options: ["Title", "Genre"]
-    }
+    it('Rendering test', () => {
 
-    const output = shallow(<CustomOptionList config={config} />);
-    expect(shallowToJson(output)).toMatchSnapshot();
- });
+        var config = {
+            title: "Search by",
+            selectedIndex: "0",
+            options: ["Title", "Genre"]
+        }
+    
+        const output = mount(<CustomOptionList config={config} />);
+        expect(shallowToJson(output)).toMatchSnapshot();
+    });
+
+    it('Click test', () => {
+
+        var config = {
+            title: "Search by",
+            selectedIndex: "0",
+            options: ["Title", "Genre"]
+        }
+    
+        const output = mount(<CustomOptionList config={config} />);
+                
+        const btn2 = output.findWhere(node => node.key() === '1').simulate('click');
+
+        expect(output.findWhere(node => node.key() === '1').hasClass(styles.isactive)).to.equal(true);
+
+        //expect(shallowToJson(output)).toMatchSnapshot();
+    });
+
+
+})
+
+
 
